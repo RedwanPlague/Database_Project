ea8a3a5f3d925bc8130213133744b5e509e0d78b
Redwanul Haque <45240975+RedwanPlague@users.noreply.github.com>
2019-02-07 23:46:33 +0600

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "host = localhost";
-    $port = "port = 5432";
-    $dbname = "dbname = HIV_Aladeen";
-    $credentials = "user = postgres password = 19yasar95";
-
-    $db = pg_connect("$host $port $dbname $credentials");
-?>
